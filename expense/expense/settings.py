5c623926e17496f73455f2747f97f4bf7bf82e46
genos0820 <prajjwal12tawri@gmail.com>
2025-01-30 19:48:47 +0530

    changes2

--- expense/expense/settings.py
+++ expense/expense/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'splitexpense',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- expense/splitexpense/views.py
+++ expense/splitexpense/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect # type: ignore
 from .forms import ExpenseForm
-from django.views import View
+from django.views import View # type: ignore
 from .models import Expense
+import datetime
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -10,11 +12,33 @@ def index(request):
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
+            
     expense_form=ExpenseForm()
     expenses=Expense.objects.all()
+    total_expenses=expenses.aggregate(Sum("amount"))
+    
+    #logic to calculate 365 days expenses
+    last_year=datetime.date.today()-datetime.timedelta(days=365)
+    data=Expense.objects.filter(date__gt=last_year)
+    yearly_sum=data.aggregate(Sum('amount'))
+    
+    #logic to calculate 30 days expenses
+    last_month=datetime.date.today()-datetime.timedelta(days=30)
+    data_m=Expense.objects.filter(date__gt=last_month)
+    monthly_sum=data_m.aggregate(Sum('amount'))
+    
+    #logic to calculate 7 days expenses
+    last_week=datetime.date.today()-datetime.timedelta(days=365)
+    data_w=Expense.objects.filter(date__gt=last_week)
+    weekly_sum=data_w.aggregate(Sum('amount'))
+    
     return render(request,"splitexpense/index.html",{
         "expense_form":expense_form,
-        "expenses": expenses
+        "expenses": expenses,
+        "total_expenses": total_expenses,
+        "yearly_sum":yearly_sum,
+        "monthly_sum":monthly_sum,
+        "weekly_sum":weekly_sum,
     })
 
 def delete(request,id):
